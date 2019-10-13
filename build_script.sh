481431c19967a277bce464221dfb7aac4f649c53
Vaisakh <vaisakhmurali@gmail.com>
2019-10-13 16:58:50 +0200

    build_script.sh : Build script added
    Signed-off-by: Vaisakh <vaisakhmurali@gmail.com>

---
+++ build_script.sh
@@ -0,0 +1,49 @@
+# !/usr/bin/sh
+rm -rf out/ kernelvv.img defconfig.log build.log
+make mrproper
+make clean
+
+VERSION=`head Makefile | grep -Po 'VERSION = \K.*'|tr -d '\n\r'`
+PATCHLEVEL=`head Makefile | grep -Po 'PATCHLEVEL = \K.*'|tr -d '\n\r'`
+SUBLEVEL=`head Makefile | grep -Po 'SUBLEVEL = \K.*'|tr -d '\n\r'`
+
+telegram-send -s sticker.webp
+
+make O=out ARCH=arm64 CROSS_COMPILE=~/aarch64gcc/bin/aarch64-linux-android- merge_hi6250_defconfig >&defconfig.log
+
+if [ $? -eq 0 ]; then
+	TIME_CURR=$(TZ=":Asia/Kolkata" date +"%d.%m.%Y-%T")
+	telegram-send $TIME_CURR" - Make "$VERSION"."$PATCHLEVEL"."$SUBLEVEL" defconfig successful"
+else
+	TIME_CURR=$(TZ=":Asia/Kolkata" date +"%d.%m.%Y-%T")
+	telegram-send $TIME_CURR" - Error in make "$VERSION"."$PATCHLEVEL"."$SUBLEVEL" defconfig building"
+fi
+
+telegram-send -f defconfig.log
+
+make O=out ARCH=arm64 CROSS_COMPILE=~/aarch64gcc/bin/aarch64-linux-android- -j200 >&build.log
+if [ $? -eq 0 ]; then
+	TIME_CURR=$(TZ=":Asia/Kolkata" date +"%d.%m.%Y-%T")
+	telegram-send $TIME_CURR" - Build "$VERSION"."$PATCHLEVEL"."$SUBLEVEL" successful"
+	telegram-send -f build.log
+	./tools/mkbootimg --kernel out/arch/arm64/boot/Image.gz --base 0x00400000 --cmdline "loglevel=4 coherent_pool=512K page_tracker=on slub_min_objects=12 unmovable_isolate1=2:192M,3:224M,4:256M printktimer=0xfff0a000,0x534,0x538 androidboot.selinux=enforcing buildvariant=user" --tags_offset 0x07A00000 --kernel_offset 0x00080000 --ramdisk_offset 0x10000000 --os_version 9 --os_patch_level 2019-04-01  --output kernelvv.img
+	id=$(gdrive upload kernelvv.img --share)
+	TOKEN=`echo $id | grep -Po 'Uploaded \K.*?(?= at)'|tr -d '\n\r'`
+	Link=`echo $id |  grep -Po 'anyone at \K.*$'|tr -d '\n\r'`
+	telegram-send 'Build Image Successful
+	Token:'$TOKEN'
+	Link: '$Link
+	rm -rf kernelvv.img
+
+else
+	TIME_CURR=$(TZ=":Asia/Kolkata" date +"%d.%m.%Y-%T")
+	telegram-send $TIME_CURR" - Error in building "$VERSION"."$PATCHLEVEL"."$SUBLEVEL
+	telegram-send -f build.log
+
+fi
+
+telegram-send -s  sticker.webp
+
+rm -rf out/ build.log defconfig.log
+make mrproper
+make clean
